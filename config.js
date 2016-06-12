9f9c3d1cb2e6d21fb23e392152a622ead73598eb
Pablo Cortez <pableocortez07@gmail.com>
2016-06-12 14:38:35 -0700

    API Keys

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports =  {
+  consumer_key:         '5qAevzqfqK3LjeI8SGXU8GEMv',
+  consumer_secret:      'valb8ocRKkWulewglgOH3sjzlboScjYa7VO5T4bCyLlr1vRlsc',
+  access_token:         '2711469817-d7EkjUh27lHcMjVeMfQFXvM7oaxh52a0DIj7z5b',
+  access_token_secret:  'hnqPzhUW1T1zR6OgM7sjWJqMtrgUvaddp0HgD9wiGPXav',
+  //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
